bf738b119c19a2996e21ee6d3a3980ef43f1c368
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-08-18 15:30:00 +0530

    Using ES6 syntax make a stop watch

--- src/index.js
+++ src/index.js
@@ -2,17 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+const watch = ReactDOM.createRoot(document.getElementById('watch'));
+var Hr=0,Min=0,Sec=0,mil=99;
+function btnStop(){
+  clearInterval(stop)
+}
+function btnStart(){
+  stop = setInterval(stopWatch,10)
+}
+function stopWatch(){
+  if(mil<99){
+    mil++;
+  }
+  else{
+    mil=0;
+    if(Sec<59){
+      Sec++;
+    }
+    else{
+      Sec=0;
+      if(Min<59){
+        Min++;
+      }
+      else{
+        Min=0;
+        if(Hr<59){
+          Hr++;
+        }
+      }
+    }
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const time =()=>{
-  root.render(
-    <div id='clock'>
-      <h2>Current time is {new Date().toLocaleTimeString()}</h2>
+watch.render(
+  <div id='main'>
+    <div id='stopWatch'>
+      <h1>{Hr}:{Min}:{Sec}:{mil}</h1>
     </div>
-  )
-};
-setInterval(time,1000);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    <div id='btn'>
+    <button type='button' onClick={btnStart}>Start</button>
+    <button type='button' onClick={btnStop}>Stop</button>
+    </div>
+  </div>
+)
+}
+
+var stop = setInterval(stopWatch,10)
+
 reportWebVitals();
