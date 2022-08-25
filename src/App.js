aefe82373860be0f3fa3a9026488304700199de0
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-08-25 12:17:11 +0530

    Q3. Repeat the above two tasks using functional components.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Greet';
 import Top from './Top';
+import GreetComp from './Components/GreetComp';
 function App() {
   return (
     <div className="App">
       <Greet name='Anshika'/>
       <Top username='Chaurasiya'/>
+      <GreetComp name='Anshika'/>
     </div>
   );
 }
---
+++ src/Components/GreetComp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GreetComp(props) {
+  return (
+    <div>
+        <h4>Hello {props.name}</h4>
+    </div>
+  )
+}
