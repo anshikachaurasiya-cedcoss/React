6f786b2784538fcf5f9d1fa6e79c84a278426ab8
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-09-01 15:59:54 +0530

    Q3 modification

--- src/Theme.js
+++ src/Theme.js
@@ -8,32 +8,35 @@ export class Theme extends Component {
       theme:''
     }
   }
-    darkTheme=()=>{
+  clickHandler=(event)=>{
+    this.name = event.target.getAttribute('name')
+    if(this.name==='dark'){
       this.theme='black'
       this.setState({
         theme:this.theme
       })
     }
-    lightTheme=()=>{
+    if(this.name==='light'){
       this.theme='light'
       this.setState({
         theme:this.theme
       })
     }
-    partyTheme=()=>{
+    if(this.name==='party'){
       this.theme='party'
       this.setState({
         theme:this.theme
       })
     }
+  }
   render() {
     return (
         <>
         Q3. Set the theme as dark, light and party on button click
         <div id='theme' className={this.theme}>
-        <button onClick={this.darkTheme}>Dark Theme</button>
-        <button onClick={this.lightTheme}>Light Theme</button>
-        <button onClick={this.partyTheme}>Party Theme</button>
+        <button onClick={this.clickHandler} name='dark'>Dark Theme</button>
+        <button onClick={this.clickHandler} name='light'>Light Theme</button>
+        <button onClick={this.clickHandler} name='party'>Party Theme</button>
       </div>
         </>
       
