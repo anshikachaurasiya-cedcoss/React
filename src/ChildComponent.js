5c4ef61b6ece3e49d6771ee1fcbd65488a9879c2
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-09-01 18:04:20 +0530

    modified code

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,34 +1,29 @@
 import React, { Component } from 'react'
 
 export class ChildComponent extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-        bg:''
-      }
+  constructor(props){
+    super(props)
+    this.state = {
+      bg:''
     }
-    changeBg=()=>{
-        if(this.props.num%2===0){
-          this.bg='red'
-          this.setState({
-            bg:this.bg
-          })
-        }
-        else{
-          this.bg='blue'
-          this.setState({
-            bg:this.bg
-          })
-        }
-      
-    }
+  }
+  
   render() {
-    return (
+    if(this.props.num%2===0){
+      this.bg="red"
+      return (
       <div id='enclosingDiv' className={this.bg}>
-        <button id='btn' onClick={this.changeBg}>Change Background</button>
+  
+      </div>)
+    }
+    else{
+      this.bg="blue"
+      return (
+      <div id='enclosingDiv' className={this.bg}>
+  
       </div>
-    )
+      )
+    }
   }
 }
 
