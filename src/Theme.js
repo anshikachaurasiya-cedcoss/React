35a97561dae34b49e0af83d452c0c2db14d91c19
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-09-01 19:38:48 +0530

    modified Q3

--- src/Theme.js
+++ src/Theme.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react'
 
 export class Theme extends Component {
+  name='light'
   constructor(props) {
     super(props)
   
     this.state = {
-      theme:''
+      theme:'',
+      name:this.name
     }
   }
   clickHandler=(event)=>{
     this.name = event.target.getAttribute('name')
-    if(this.name==='dark'){
-      this.theme='black'
-      this.setState({
-        theme:this.theme
-      })
-    }
-    if(this.name==='light'){
-      this.theme='light'
-      this.setState({
-        theme:this.theme
-      })
-    }
-    if(this.name==='party'){
-      this.theme='party'
-      this.setState({
-        theme:this.theme
-      })
-    }
+    this.setState({
+      name:this.name
+    })
   }
   render() {
-    return (
-        <>
-        Q3. Set the theme as dark, light and party on button click
-        <div id='theme' className={this.theme}>
-        <button onClick={this.clickHandler} name='dark'>Dark Theme</button>
-        <button onClick={this.clickHandler} name='light'>Light Theme</button>
-        <button onClick={this.clickHandler} name='party'>Party Theme</button>
-      </div>
-        </>
-      
-    )
+    if(this.state.name==='dark'){
+      this.theme='black'
+      this.element =(<div id='theme' className={this.theme}>
+      <button onClick={this.clickHandler} name='dark'>Dark Theme</button>
+      <button onClick={this.clickHandler} name='light'>Light Theme</button>
+      <button onClick={this.clickHandler} name='party'>Party Theme</button>
+    </div>)
+    }
+    if(this.state.name==='light'){
+      this.theme='light'
+      this.element = (<div id='theme' className={this.theme}>
+      <button onClick={this.clickHandler} name='dark'>Dark Theme</button>
+      <button onClick={this.clickHandler} name='light'>Light Theme</button>
+      <button onClick={this.clickHandler} name='party'>Party Theme</button>
+    </div>)
+    }
+    if(this.state.name==='party'){
+      this.theme='party'
+      this.element = (<div id='theme' className={this.theme}>
+      <button onClick={this.clickHandler} name='dark'>Dark Theme</button>
+      <button onClick={this.clickHandler} name='light'>Light Theme</button>
+      <button onClick={this.clickHandler} name='party'>Party Theme</button>
+    </div>)
+    }
+    return <>{this.element}</>
   }
 }
 
