5426019e290fb4180028eb6c9b07bda527cb0a7e
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-10-11 14:44:57 +0530

    library app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import LandingPage from './Components/LandingPage';
 
 function App() {
+  var value=''
+  const [arr,setArr]=useState([])
+  var inpVal=useRef()
+  
+  useEffect(() => {
+    if (inpVal.current) {
+      inpVal.current.focus();
+    }
+  }, []);
+
+  const search=()=>{
+    value = inpVal.current.value
+    fetch('https://openlibrary.org/search.json?q=fantastic+mr+fox&mode=ebooks&has_fulltext=true')
+    .then(res=>res.json())
+    .then(data=>{
+        setArr(data.docs)
+    })
+  }
+  
+  const divhandler=(seed)=>{
+    fetch('https://openlibrary.org/api/books?bibkeys=ISBN:'+seed+'&jscmd=details&format=json')
+    .then(res=>res.json())
+    .then(data=>{
+      let x='ISBN:'+seed;
+      window.location.href=data[x].info_url
+    })
+  }
+
   return (
-     <div className='main'>
-        <LandingPage/>
+    <>
+    <div className='main'>
+      <LandingPage arr={arr} search={search} div={divhandler} val={inpVal}/>
     </div>
+    </>    
   );
 }
 
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,40 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from "react"
 
-const LandingPage = () => {
-    const [arr,setArr]=useState([])
-    var inpVal=useRef()
-    const search=()=>{
-        fetch('https://openlibrary.org/search.json?q=fantastic+mr+fox&mode=ebooks&has_fulltext=true')
-        .then(res=>res.json())
-        .then(data=>{
-            setArr(data.docs)
-            console.log(data)
-            console.log(data.docs)
-        })
-    }
-    useEffect(() => {
-        if (inpVal.current) {
-          inpVal.current.focus();
-        }
-      }, []);
-      
-      return (
-      <div className='landingPage'>
+const LandingPage = (props) => {
+    
+    return (
+    <div className='landingPage'>
         <div className='landingPage_innerdiv1'>
             <div className='landingpage_right'>
                 <h1>Room Without books is like a body without soul...</h1>
                 <img src='pic.png' alt=''/>
             </div>
             <div className='landingpage_right'>
-                <input ref={inpVal}/>
-                <button onClick={search}>Search</button>
+                <input ref={props.val}/>
+                <button onClick={props.search}>Search</button>
             </div>
         </div>
-        <div className='list'>
-            {arr.map((item)=>{
+        <div className='list' >
+            {props.arr.map((item,i)=>{
                 return(
-                <div className='item'>
+                <div className='item' ind={i} onClick={()=>props.div(item.isbn[0])}>
                     <p>{item.title}</p>
+                    <img src={`https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`} alt=""/>
+                    <p>{item.ebook_access}</p>
                 </div>
                 )
             })}
