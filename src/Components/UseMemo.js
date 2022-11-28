68d8e00a1b41d16cecb661d24bdd06cf79f7165a
anshikachaurasiya-cedcoss <anshikachaurasiya@cedcoss.com>
2022-11-28 14:37:13 +0530

    Use Memo

--- src/Components/UseMemo.js
+++ src/Components/UseMemo.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import Comp1Memo from './Comp1Memo'
+import React, { useMemo, useRef, useState } from 'react'
 
 const UseMemo = () => {
+    var refInp1 = useRef('')
+    var refInp2 = useRef('')
+    var [sum,setSum]=useState()
+    var [style,setStyle]=useState('green')
+
+    // Multiply function using usememo
+    
+    var mult =useMemo(function multiply(){
+        console.log('Multiply Function')
+        return parseInt(refInp1.current.value)*parseInt(refInp2.current.value)
+    },[refInp1.current.value,refInp2.current.value])
+
+    // add function
+
+    const add=()=>{
+        sum = parseInt(refInp1.current.value)+parseInt(refInp2.current.value)
+        setSum(sum)
+    }
+
+    // button toggle function
+
+    const toggles=()=>{
+        if(style=='green'){
+        setStyle('yellow')
+        }
+        else{
+            setStyle('green')
+        }
+    }
+
   return (
     <div>
-        <Comp1Memo/>
+        <div>
+        <input  type='number' placeholder='input 1' ref={refInp1}/>
+        <input  type='number' placeholder='input 2' ref={refInp2}/>
+        <button onClick={add}>Add Button</button>
+    </div>
+    Sum is:{sum}<br/>
+    Multiply is:{mult}<br/>
+    <button onClick={toggles} className={style}>Toggle Button</button>
     </div>
   )
 }
